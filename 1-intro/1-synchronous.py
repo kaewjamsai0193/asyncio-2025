10047257a04645a18e4f1cfc3a2aaf26b1666d4e
kaewjamsai0193 <107392961+kaewjamsai0193@users.noreply.github.com>
2025-06-11 10:48:46 +0700

    3 to 5

--- 1-intro/1-synchronous.py
+++ 1-intro/1-synchronous.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 def make_burger(student_id):
     print(f"[{datetime.now().strftime('%H:%M:%S')}] เริ่มทำเบอร์เกอร์ให้นักเรียนคนที่ {student_id}")
-    
+
     print(f"[{datetime.now().strftime('%H:%M:%S')}] 1. ทอดเบอร์เกอร์...")
     time.sleep(5)
 
@@ -20,10 +20,10 @@ def make_burger(student_id):
 
 def main():
     start = time.time()
-    
-    for i in range(1, 4):
+
+    for i in range(1, 6):
         make_burger(i)
-    
+
     end = time.time()
     print(f"[{datetime.now().strftime('%H:%M:%S')}] รวมเวลาทั้งหมด: {end - start:.2f} วินาที")
 
