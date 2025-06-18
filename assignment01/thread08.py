9a448768bba9053063f09040557b6993fa474405
kaewjamsai0193 <107392961+kaewjamsai0193@users.noreply.github.com>
2025-06-18 11:24:44 +0700

    thread08

--- assignment01/thread08.py
+++ assignment01/thread08.py
@@ -7,6 +7,7 @@ import time
 class FakeDatabase:
     def __init__(self):
         self.value = 0
+
     def update(self, name):
         logging.info("Thread %s: starting update", name)
         local_copy = self.value
@@ -20,8 +21,8 @@ if __name__ == "__main__":
     logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
 
     database = FakeDatabase()
-    logging.info("testing update. Strting value is %d", database.value)
+    logging.info("Testing update. Strting value is %d", database.value)
     with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
         for index in range(2):
             executor.submit(database.update, index)
-        logging.info("Testing update Ending value is %d", database.value)
+    logging.info("Testing update Ending value is %d", database.value)
