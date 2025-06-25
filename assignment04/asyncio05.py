956128722aa40b401b1a5acad8a5fd8ba4a65aaa
kaewjamsai0193 <107392961+kaewjamsai0193@users.noreply.github.com>
2025-06-25 11:55:34 +0700

    assignment

--- assignment04/asyncio05.py
+++ assignment04/asyncio05.py
@@ -1,1 +1,14 @@
 # Check if a Task is Done
+import asyncio
+
+async def simple_task():
+    await asyncio.sleep(1)
+    return "เสร็จสิ้น"
+
+async def main():
+     task = asyncio.create_task(simple_task())
+     print("ก่อน await:", task.done())
+     await task
+     print("หลัง await:", task.done())
+
+asyncio.run(main())
