bafa5c3e753c09b9e2033014a5ceac5535ab52fa
kaewjamsai0193 <107392961+kaewjamsai0193@users.noreply.github.com>
2025-06-25 10:59:36 +0700

    async

--- assignment04/asyncio03.py
+++ assignment04/asyncio03.py
@@ -1,2 +1,16 @@
-# Create 2 Tasks with High-Level API
+# Create 2 Tasks with High-Level API\
+import asyncio
 
+async def dowload_image(name, delay):
+    print(f"{name} กำลังโหลด...")
+    await asyncio.sleep(delay)
+    print(f"{name} โหลดเสร็จแล้ว!")
+
+async def main():
+    task1 = asyncio.create_task(dowload_image("ภาพที่ 1 ", 2))
+    task2 = asyncio.create_task(dowload_image("ภาพที่ 2 ", 3))
+
+    await task1
+    await task2
+
+asyncio.run(main())
--- assignment04/asyncio04.py
+++ assignment04/asyncio04.py
@@ -1,3 +1,20 @@
 # example of starting many tasks and getting access to all tasks
+import time
+import asyncio
 
+
+async def dowload_image(name, delay):
+    print(f"{time.ctime()} {name} กำหลังโหลด...")
+    await asyncio.sleep(delay)
+    print(f"{time.ctime()} {name} โหลดเสร็จแล้ว!")
+
+async def main():
+    print(f"{time.ctime()} main coroutine started")
+
+    started_tasks = [asyncio.create_task(dowload_image(i, i)) for i in range(3)]
+
+    await asyncio.sleep(0.1)
+    for task in started_tasks:
+        await task
+asyncio.run(main())
 
