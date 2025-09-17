50e70524a4c27077f1985f74858f316292f0a240
kaewjamsai0193 <107392961+kaewjamsai0193@users.noreply.github.com>
2025-09-17 10:24:28 +0700

    exam-02

--- exam-02/exam02.py
+++ exam-02/exam02.py
@@ -38,12 +38,15 @@ async def worker(id: int):
 
 async def main():
     tasks = []
-    
+
     # TODO: สร้าง asyncio task สำหรับ worker 3 ตัว
     # hint: ใช้ asyncio.create_task(worker(id))
-    
+    for i in range(1,4):
+        tasks.append(asyncio.create_task(worker(i)))
+
     # TODO: รอให้ทุก task เสร็จ
     # hint: ใช้ await หรือ asyncio.gather
+    await asyncio.gather(*tasks)
     pass
 
 asyncio.run(main())
